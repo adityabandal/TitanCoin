cc8c0d8211d3d98ce973356547cc600c2fff103d
adityabandal <adityabandal007@gmail.com>
2021-05-02 11:14:06 +0530

    initial commit

---
+++ src/app/services/blockchain.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { from } from 'rxjs';
+import { Blockchain } from 'blockchain/src/blockchain';
+import EC from 'elliptic';
+@Injectable({
+  providedIn: 'root',
+})
+export class BlockchainService {
+  public blockchainInstance = new Blockchain();
+
+  public walletKeys = [];
+  constructor() {
+    this.blockchainInstance.difficulty = 1;
+    this.blockchainInstance.minePendingTransactions('my-wallet-address');
+
+    this.generateWalletKeys();
+  }
+
+  private generateWalletKeys() {
+    const ec = new EC.ec('secp256k1');
+    const key = ec.genKeyPair();
+
+    this.walletKeys.push({
+      keyObj: key,
+      publicKey: key.getPublic('hex'),
+      privateKey: key.getPrivate('hex'),
+    });
+  }
+}
