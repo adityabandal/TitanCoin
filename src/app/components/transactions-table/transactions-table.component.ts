aa690d5600a709eb1012a3c6a42b8c2a4d8415b6
adityabandal <adityabandal007@gmail.com>
2021-05-11 22:23:28 +0530

    added transactions table

---
+++ src/app/components/transactions-table/transactions-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { BlockchainService } from 'src/app/services/blockchain.service';
+
+@Component({
+  selector: 'app-transactions-table',
+  templateUrl: './transactions-table.component.html',
+  styleUrls: ['./transactions-table.component.scss'],
+})
+export class TransactionsTableComponent implements OnInit {
+  @Input()
+  public transactions = [];
+  constructor(public blockchainService: BlockchainService) {}
+
+  ngOnInit(): void {}
+}
